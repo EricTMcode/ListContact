cb8590dd3c9ddb1409c3bdc234926a09f74631fb
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-30 14:55:49 +0200

    - Refactor ListContactViewModel

--- ListContact/ViewModels/ListContactViewModel.swift
+++ ListContact/ViewModels/ListContactViewModel.swift
@@ -5,57 +5,56 @@
 //  Created by Eric on 23/05/2023.
 //
 
-import Foundation
+import SwiftUI
 
-class ListContactVieModel: ObservableObject {
+class ListContactViewModel: ObservableObject {
     @Published var contacts: [Contact] = []
     
     init() {
 //        contacts = Contact.example
-        loadData()
+        loadContacts()
     }
     
-    func saveContact(name: String, location: String, jpegData: Data) {
-        let newContact = Contact(name: name, location: location, jpegData: jpegData)
-        contacts.append(newContact)
-        saveData()
-    }
-    
-    func updateContact(contact: Contact, name: String, location: String) {
-        if let index = contacts.firstIndex(where: {$0.id == contact.id}) {
-            contacts[index].name = name
-            contacts[index].location = location
-            saveData()
+    func addContact(_ name: String, location: String, image: UIImage) {
+        let newContact = Contact(name: name, location: location)
+        do {
+            try FileManager().saveImage("\(newContact.id)", image: image)
+            contacts.append(newContact)
+            saveContacts()
+        } catch {
+            fatalError("Oups")
         }
-    }
-    
-    func removeContact(indexSet: IndexSet) {
-        contacts.remove(atOffsets: indexSet)
-        saveData()
     }
     
     //MARK: - DOCUMENTS DIRECTORY
     
-    let filURL = URL.documentsDirectory.appending(component: "contacts.json")
-
-    func saveData() {
+    func saveContacts() {
+        let encoder = JSONEncoder()
         do {
-            let contactsListData = try JSONEncoder().encode(contacts)
-            let contactsListString = String(decoding: contactsListData, as: UTF8.self)
-            try contactsListString.write(to: filURL, atomically: true, encoding: .utf8)
+            let data = try encoder.encode(contacts)
+            let jsonString = String(decoding: data, as: UTF8.self)
+            do {
+                try FileManager().saveDocument(contents: jsonString)
+            } catch {
+                fatalError("Oups")
+            }
         } catch {
-            fatalError("😡 ERROR: Could not save data \(error.localizedDescription)")
+            fatalError("Oups")
         }
     }
     
-    func loadData() {
-        if FileManager().fileExists(atPath: filURL.path) {
+    func loadContacts() {
+        do {
+            let data = try FileManager().readDocuments()
+            let decoder = JSONDecoder()
             do {
-                let data = try Data(contentsOf: filURL)
-                contacts = try JSONDecoder().decode([Contact].self, from: data)
+                contacts = try decoder.decode([Contact].self, from: data)
             } catch {
-                saveData()
+                fatalError("Oups")
             }
+        } catch {
+            fatalError("Oups")
         }
     }
 }
+
